1ed2ce0e19d03f52826a20618f253425207ab027
Sulayman Matkuliyev <suleymantm11@gmail.com>
2022-01-14 21:28:28 +0300

    14.01.2022

---
+++ src/minefield/Runner.java
@@ -0,0 +1,23 @@
+package minefield;
+
+import java.util.Scanner;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Welcome to MineField");
+		System.out.println("Please enter minefield dimensions");
+		System.out.print("Enter row length : ");
+		int row = scan.nextInt();
+		System.out.print("Enter column height : ");
+		int column = scan.nextInt();
+		
+		MineField mine = new MineField(row, column);
+		mine.run();
+		
+		scan.close();
+	}
+
+}
